334e3eb0037ef411bb46aca75425a9d60f33094d
Yannick Visbeek <yvisbeek@gmail.com>
2017-12-18 22:17:36 +0100

    images now saving

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const fs = require('fs')
+const uuidv4 = require('uuid/v4')
 
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit: '20mb'}))
+app.use(bodyParser.urlencoded({limit: '5mb'}))
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.post('/postImage', (req, res) => {
-    console.log(req.body.data)
-    res.send('hi')
+
+    var base64Data = req.body.data.replace(/^data:image\/png;base64,/, "");
+
+    fs.writeFile(`images/${uuidv4()}.png`, base64Data, 'base64', function(err) {
+        if (err) console.log(err);
+    });
 })
 
 app.listen(port, () => {
