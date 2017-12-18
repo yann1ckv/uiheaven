1c9edc57ce5e869261925fd7b97b24ea38d97922
Yannick Visbeek <yvisbeek@gmail.com>
2017-12-18 12:39:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
