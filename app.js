a4e5ef04ec580ed9349f63399c2480fc290b9119
Alexandra Briceag <Computer1@Alexandras-MacBook-Pro-2.local>
2017-12-22 01:34:48 +0100

    Communistic Style Sheet

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env nodejs
 
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.PORT
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const uuidv4 = require('uuid/v4')
+const { Client } = require('pg')
 
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -17,17 +20,48 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/mylife', (req, res) => {
-    res.render('snitch')
+app.get('/images', (req, res) => {
+    const client = new Client({
+        database: process.env.DATABASE,
+        host: process.env.HOST,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        port: process.env.DBPORT
+    })
+
+    client.connect()
+
+    client.query('SELECT image FROM complaints')
+    .then(result => {
+        res.render('images', {images: result.rows})
+    })
+    .catch(err => console.log(err))
 })
 
-app.post('/postImage', (req, res) => {
+app.post('/mylife', (req, res) => {
+    console.log('reached')
+    const client = new Client({
+        database: process.env.DATABASE,
+        host: process.env.HOST,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        port: process.env.DBPORT
+    })
+
+    client.connect()
 
     var base64Data = req.body.data.replace(/^data:image\/png;base64,/, "");
 
-    fs.writeFile(`images/${uuidv4()}.png`, base64Data, 'base64', function(err) {
+    var name = uuidv4()
+
+    fs.writeFile(`public/images/${name}.png`, base64Data, 'base64', function(err) {
         if (err) console.log(err)
     })
+
+    client.query(`INSERT INTO complaints (name, postcode, image, email) VALUES ('Döner masters', '1077 VS', '${name}', 'lookatmesnitching@gmail.com')`)
+    .catch(err => console.log(err))
+
+    res.render('snitch')
 })
 
 app.listen(port, () => {
